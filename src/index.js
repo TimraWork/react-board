979c55b146fc94922d40a5052e2d68848d01038d
Мухамеджанова Эльмира <timra.work@gmail.com>
2020-04-20 18:15:42 +0500

    Create simple components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('asdf');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const TodoList = () => {
+	return (
+		<ul>
+			<li>Learn React</li>
+			<li>Build Awesome App</li>
+		</ul>
+	);
+};
+
+const AppHeader = () => {
+	return <h1>My Todo list</h1>;
+};
+
+const SearchPanel = () => {
+	return <input type="text" placeholder="search" />;
+};
+
+const el = (
+	<div>
+		<SearchPanel />
+		<AppHeader />
+		<TodoList />
+	</div>
+);
+
+ReactDom.render(el, document.getElementById('root'));
